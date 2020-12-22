b4d075f48a4c3677801c177dfa26f876b460e4b4
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-21 17:21:52 -0700

    drag and drop script

--- script.js
+++ script.js
@@ -3,31 +3,37 @@ const empties = document.querySelectorAll('.empty')
 
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
-fill.addEventListener('dragover', dragOver)
-fill.addEventListener('dragenter', dragEnter)
-fill.addEventListener('dragleave', dragLeave)
-fill.addEventListener('dragdrop', dragDrop)
+
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver)
+  empty.addEventListener('dragenter', dragEnter)
+  empty.addEventListener('dragleave', dragLeave)
+  empty.addEventListener('drop', dragDrop)
+}
 
 function dragStart() {
-
+  this.className += ' hold'
+  setTimeout(() => this.className = 'invisible', 0)
 }
 
 function dragEnd() {
-
+  this.className = 'fill'
 }
 
-function dragOver() {
-
+function dragOver(e) {
+  e.preventDefault()
 }
 
-function dragEnter() {
-
+function dragEnter(e) {
+  e.preventDefault()
+  this.className += ' hovered'
 }
 
 function dragLeave() {
-
+  this.className = 'empty'
 }
 
 function dragDrop() {
-
+  this.className = 'empty'
+  this.append(fill)
 }
