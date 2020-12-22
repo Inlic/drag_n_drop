01789b4a423f35813c48243d76198600bc35b456
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-21 17:15:31 -0700

    multiple listeners

---
+++ script.js
@@ -0,0 +1,33 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+fill.addEventListener('dragover', dragOver)
+fill.addEventListener('dragenter', dragEnter)
+fill.addEventListener('dragleave', dragLeave)
+fill.addEventListener('dragdrop', dragDrop)
+
+function dragStart() {
+
+}
+
+function dragEnd() {
+
+}
+
+function dragOver() {
+
+}
+
+function dragEnter() {
+
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+
+}
